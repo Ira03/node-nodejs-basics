95faefbeeac7df91ae0b8be758f6936b3edb21dd
Irina Yakubenia <i.yakubenia@gmail.com>
2023-04-08 20:52:14 +0300

    Read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { readFile } from "node:fs/promises";
+
+import { createFilePath } from "../utils/index.js";
+
+import { ERROR_MESSAGE } from "../constants/index.js";
+
 const read = async () => {
-    // Write your code here 
+  const path = createFilePath("files", "fileToRead.txt");
+  try {
+    const content = await readFile(path, "utf8");
+    console.log(content);
+  } catch (e) {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await read();
