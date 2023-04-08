010eb7c1b91dde8944c0e2365bb1dd80b0a3157c
Irina Yakubenia <i.yakubenia@gmail.com>
2023-04-09 00:16:08 +0300

    ParseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const result = args
+    .reduce((acc, item, i, array) => {
+      if (item.startsWith("--")) {
+        const key = item.replace("--", "");
+        const value = array[i + 1];
+        acc.push(`${key} is ${value}`);
+      }
+      return acc;
+    }, [])
+    .join(", ");
+
+  console.log(result);
 };
 
 parseArgs();
