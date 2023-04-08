ea336b590a02849dfb7778ca07dcc0b67429a4cb
Irina Yakubenia <i.yakubenia@gmail.com>
2023-04-08 20:05:15 +0300

    Delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
+import { unlink } from "node:fs/promises";
+
+import { createFilePath } from "../utils/index.js";
+
+import { ERROR_MESSAGE, NO_SUCH_FILE_ERROR_CODE } from "../constants/index.js";
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = createFilePath("files", "fileToRemove.txt");
+
+  try {
+    await unlink(filePath);
+  } catch (e) {
+    if (e.code === NO_SUCH_FILE_ERROR_CODE) {
+      throw new Error(ERROR_MESSAGE);
+    }
+  }
 };
 
 await remove();
