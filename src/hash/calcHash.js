8f516af8ce9528b7299ff1707934886f5524bbd4
Irina Yakubenia <i.yakubenia@gmail.com>
2023-04-09 01:52:54 +0300

    Hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { readFile } from "node:fs/promises";
+import { createHash } from "crypto";
+import { fileURLToPath, pathToFileURL } from "node:url";
+import path from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+
+  const filePath = path.resolve("src/hash/files", "fileToCalculateHashFor.txt");
+
+  try {
+    const content = await readFile(filePath);
+    const hashHex = hash.update(content).digest("hex");
+    console.log(hashHex);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 await calculateHash();
