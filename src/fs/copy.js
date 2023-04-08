52df2882768aea28a1f88dcf8fc6fccf4adfb608
Ira03 <i.yakubenia@gmail.com>
2023-04-08 19:06:32 +0300

    Copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import { cp } from "node:fs/promises";
+
+import { checkIsFileExist, createFilePath } from "../utils/index.js";
+import { ERROR_MESSAGE } from "../constants/index.js";
+
 const copy = async () => {
-    // Write your code here 
+  const src = createFilePath("files");
+  const dest = createFilePath("files_copy");
+
+  const isSrcExist = await checkIsFileExist(src);
+  const isDestExist = await checkIsFileExist(dest);
+
+  if (!isSrcExist || isDestExist) {
+    throw new Error(ERROR_MESSAGE);
+  }
+  await cp(src, dest, { recursive: true });
 };
 
 await copy();
