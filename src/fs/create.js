bfceb73663c575235c59872ba740c00529f48753
Irina Yakubenia <i.yakubenia@gmail.com>
2023-04-08 20:05:43 +0300

    Add constant to create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,15 +1,22 @@
 import { writeFile } from "node:fs/promises";
 
 import { createFilePath } from "../utils/index.js";
-import { CONTENT, ERROR_MESSAGE } from "../constants/index.js";
+
+import {
+  CONTENT,
+  ERROR_MESSAGE,
+  FILE_EXISTS_ERROR_CODE,
+} from "../constants/index.js";
 
 const create = async () => {
   const filePath = createFilePath("files", "fresh.txt");
 
   try {
     await writeFile(filePath, CONTENT, { encoding: "utf8", flag: "wx" });
-  } catch {
-    throw new Error(ERROR_MESSAGE);
+  } catch (e) {
+    if (e.code === FILE_EXISTS_ERROR_CODE) {
+      throw new Error(ERROR_MESSAGE);
+    }
   }
 };
 
