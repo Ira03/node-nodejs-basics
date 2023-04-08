fd2e161fb4bea10ece4f655e4b3e3fcc768805ef
Irina Yakubenia <i.yakubenia@gmail.com>
2023-04-08 20:28:59 +0300

    List function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { readdir } from "node:fs/promises";
+
+import { createFilePath } from "../utils/index.js";
+
+import { ERROR_MESSAGE, NO_SUCH_FILE_ERROR_CODE } from "../constants/index.js";
+
 const list = async () => {
-    // Write your code here 
+  const path = createFilePath("files");
+
+  try {
+    const filesNamesList = await readdir(path);
+    console.log(filesNamesList);
+  } catch (e) {
+    if (e.code === NO_SUCH_FILE_ERROR_CODE) {
+      throw new Error(ERROR_MESSAGE);
+    }
+  }
 };
 
 await list();
