f9bdc5179217da35b37e9100c0b34e76cd3b75ba
Irina Yakubenia <i.yakubenia@gmail.com>
2023-04-08 21:37:33 +0300

    Add flags to cp

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,21 +1,17 @@
 import { cp } from "node:fs/promises";
 import path from "path";
 
-import { checkIsFileExist } from "../utils/index.js";
 import { ERROR_MESSAGE } from "../constants/index.js";
 
 const copy = async () => {
   const src = path.resolve("src/fs/files");
   const dest = path.resolve("src/fs/files_copy");
 
-  const isSrcExist = await checkIsFileExist(src);
-  const isDestExist = await checkIsFileExist(dest);
-
-  if (!isSrcExist || isDestExist) {
+  try {
+    await cp(src, dest, { recursive: true, force: false, errorOnExist: true });
+  } catch {
     throw new Error(ERROR_MESSAGE);
   }
-
-  await cp(src, dest, { recursive: true });
 };
 
 await copy();
