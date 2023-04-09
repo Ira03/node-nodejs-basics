bd3d7a3d24c7abe53f2b0fda0a7cc50005a64add
Irina Yakubenia <i.yakubenia@gmail.com>
2023-04-09 20:11:03 +0300

    Workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,24 @@
+import os from "os";
+import path from "path";
+import { Worker } from "worker_threads";
+
 const performCalculations = async () => {
-    // Write your code here
+  const systemCpuCores = os.cpus();
+
+  const filePath = path.resolve("src/wt/worker.js");
+
+  const promises = systemCpuCores.map(
+    (_, i) =>
+      new Promise((resolve) => {
+        const worker = new Worker(filePath, { workerData: 10 + i });
+
+        worker.on("message", (data) => resolve({ status: "resolved", data }));
+        worker.on("error", () => resolve({ status: "error", data: null }));
+      })
+  );
+
+  const result = await Promise.all(promises);
+  console.log(result);
 };
 
 await performCalculations();
