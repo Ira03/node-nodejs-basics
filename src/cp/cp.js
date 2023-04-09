8c9b75a4d7ddd74ffe6671d12efa217bd6f84603
Irina Yakubenia <i.yakubenia@gmail.com>
2023-04-09 21:28:42 +0300

    Child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import { fork } from "node:child_process";
+import path from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const filePath = path.resolve("src/cp/files/script.js");
+  const child = fork(filePath, args, { silent: true });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["hello", "world"]);
