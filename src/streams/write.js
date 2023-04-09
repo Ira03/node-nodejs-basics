3e6564cef1f18141bd3ffa57388d9f8c704f8a82
Irina Yakubenia <i.yakubenia@gmail.com>
2023-04-09 13:42:56 +0300

    Write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,11 @@
+import { createWriteStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+import path from "path";
+
 const write = async () => {
-    // Write your code here 
+  const filePath = path.resolve("src/streams/files", "fileToWrite.txt");
+
+  await pipeline(process.stdin, createWriteStream(filePath, { flags: "a" }));
 };
 
 await write();
